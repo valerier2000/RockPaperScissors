b932b3aaa251afde8a43cb1a97fd96a1bf17e2d4
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-04-05 20:57:52 +0200

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Valeriia Rohatynska on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
